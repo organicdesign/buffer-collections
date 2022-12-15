40b8cae51df281fa8096fa3636751820df7e3cd5
saul <saul@organicdesign.co.nz>
2022-12-16 12:00:40 +1300

    Build.

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,8 @@
 import { toString as uint8ArrayToString } from "uint8arrays/to-string";
 import { fromString as uint8ArrayFromString } from "uint8arrays/from-string";
+const toString = (data) => uint8ArrayToString(data, "ascii");
+const fromString = (data) => uint8ArrayFromString(data, "ascii");
 export default class BufferMap {
-    constructor(iterable) {
-        this.data = new Map();
-        if (iterable != null) {
-            this.data = new Map((function* () {
-                for (const [buf, value] of iterable) {
-                    yield [uint8ArrayToString(buf), value];
-                }
-            })());
-        }
-    }
     get [Symbol.toStringTag]() {
         return this.data[Symbol.toStringTag];
     }
@@ -18,29 +10,39 @@ export default class BufferMap {
         const itr = this.data.entries();
         return (function* () {
             for (const [str, value] of itr) {
-                yield [uint8ArrayFromString(str), value];
+                yield [fromString(str), value];
             }
         })();
+    }
+    constructor(iterable) {
+        this.data = new Map();
+        if (iterable != null) {
+            this.data = new Map((function* () {
+                for (const [buf, value] of iterable) {
+                    yield [toString(buf), value];
+                }
+            })());
+        }
     }
     clear() {
         this.data.clear();
     }
     delete(key) {
-        return this.data.delete(uint8ArrayToString(key));
+        return this.data.delete(toString(key));
     }
     forEach(callbackfn, thisArg) {
         this.data.forEach((value, key) => {
-            callbackfn.apply(thisArg, [value, uint8ArrayFromString(key), this]);
+            callbackfn.apply(thisArg, [value, fromString(key), this]);
         });
     }
     get(key) {
-        return this.data.get(uint8ArrayToString(key));
+        return this.data.get(toString(key));
     }
     has(key) {
-        return this.data.has(uint8ArrayToString(key));
+        return this.data.has(toString(key));
     }
     set(key, value) {
-        this.data.set(uint8ArrayToString(key), value);
+        this.data.set(toString(key), value);
         return this;
     }
     entries() {
@@ -53,7 +55,7 @@ export default class BufferMap {
         const itr = this.data.keys();
         return (function* () {
             for (const key of itr) {
-                yield uint8ArrayFromString(key);
+                yield fromString(key);
             }
         })();
     }
