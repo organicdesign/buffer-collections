506fc7d5cd1196424cff97f1a4251776a8032300
saul <saul@organicdesign.co.nz>
2022-12-16 11:29:59 +1300

    Test if the type is the same as a standard map.

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,3 +1,11 @@
-describe("Empty test", () => {
-	it("runs", () => {});
+import BufferMap from "../src/index.js";
+import dataSet from "./test-data.js";
+
+describe("type", () => {
+	it("has the same toStringTag as map", () => {
+		const bm = new BufferMap();
+		const m = new Map();
+
+		expect(m[Symbol.toStringTag]).toStrictEqual(bm[Symbol.toStringTag]);
+	});
 });
